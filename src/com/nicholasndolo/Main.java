214f0c91ca334229f3e4be5c07ed3a2a9c38157a
Nicholas <nickndolo95@gmail.com>
2024-10-09 15:34:42 +0300

    implement a priority queue with add method

---
+++ src/com/nicholasndolo/Main.java
@@ -0,0 +1,31 @@
+package com.nicholasndolo;
+
+
+public class Main {
+    public static void main(String[] args) {
+//        PriorityQueue<String> queque = new PriorityQueue<>();
+//        queque.add("Rose");
+//        queque.add("Jenny");
+//        queque.add("Adrian");
+//        queque.add("Cate");
+//        queque.add("Mwende");
+//
+//
+//
+//        while(!queque.isEmpty())
+//            System.out.println(queque.remove());
+
+        PriorityQueue queque = new PriorityQueue();
+        queque.add(5);
+        queque.add(2);
+        queque.add(6);
+        queque.add(3);
+        queque.add(4);
+        queque.add(2);
+
+
+
+        System.out.println(queque);
+
+    }
+}
